1d3d536da3be79e887bae53904db953f1f6873c3
CarlosEduardoBalbastro <balbastrocae@gmail.com>
2022-09-16 21:09:46 -0300

    angular tres mil ejercicio taskList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listaTareasPractica';
+  todos : Todo[] - [];
+  newTodo : string;
+  i:number;
 }
